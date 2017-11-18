61a4690fe9cc15a255252376a773ae94ecb9b53c
Jeremy <junkofly@gmail.com>
2017-11-17 22:24:23 -0500

    Created classes

---
+++ TP3_Libs/Tree/Tree.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "TreeElement.h"
+#include <iostream>
+
+using namespace std;
+class Tree {
+	public:
+		Tree();
+		~Tree();
+		void add(TreeElement* e);
+		bool isEmpty();
+		int searchElement(TreeElement* e);
+		int nbOfElements();
+		int getDepth(); //BONUS
+	private:
+		struct Node {
+			public:
+				Node();
+			private:
+				TreeElement* _element;
+				Node* _parent;
+				Node* _left;
+				Node* _right;
+		};
+		Node* _root;
+};
